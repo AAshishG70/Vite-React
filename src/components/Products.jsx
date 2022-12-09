e8b2b521e69a4ab781c279698e19f2193a77794c
Aashish Gubhaju <aashishgubhaju12@gmail.com>
2022-12-08 19:51:10 -0700

    Add Products

---
+++ src/components/Products.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductDetail from "./ProductDetail";
+
+const Products = () => {
+  return (
+    <div>
+      <ProductDetail />
+    </div>
+  );
+};
+
+export default Products;
